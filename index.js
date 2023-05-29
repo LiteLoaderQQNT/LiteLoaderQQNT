06cf1af655de6881a42ebdaa4df0dd0fbb2db762
沫烬染 <mojinran@outlook.com>
2023-05-29 21:32:07 +0800

    优化代码，Hook BrowserWindow

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
-const { BrowserWindow } = require("electron");
+const { Module } = require("module");
 const { BetterQQNTLoader } = require("./loader.js");
 const base = require("./base.js");
 
 
-// 初始化，加载BetterQQNT
-function initialization(webContents) {
-    const loader = new BetterQQNTLoader(webContents);
-    base.output("BetterQQNTLoader Created.");
-    loader.loadAPI();
-    base.output("API Loaded.");
-    loader.loadPlugins();
-    base.output("loadPlugins Loaded.");
-    loader.loadRendererLoader();
-    base.output("loadRendererLoader Loaded.");
+// 初始化
+function initialization(callback) {
+    const original_load = Module._load;
+    Module._load = (...args) => {
+        const loaded_module = original_load(...args);
+
+        if (args[0] === "electron") {
+            function HookedBrowserWindow(original_config) {
+                const config = {
+                    ...original_config,
+                    webPreferences: {
+                        ...original_config?.webPreferences,
+                        devTools: true,
+                    },
+                };
+                const window = new loaded_module.BrowserWindow(config);
+                callback(window);
+                return window;
+            }
+            return {
+                ...loaded_module,
+                BrowserWindow: HookedBrowserWindow,
+            }
+        }
+
+        return loaded_module;
+    };
 }
 
 
-// 查找窗口
-const interval = setInterval(() => {
-    const getAllWindows = BrowserWindow.getAllWindows();
-    for (const browserWindow of getAllWindows) {
-        const webContents = browserWindow.webContents;
-        const url = webContents.getURL();
-        if (url.includes("app://./renderer/index.html")) {
-            base.output("Found Index Window!");
-            clearInterval(interval);
-            base.output("Initializing...");
-            initialization(webContents);
-            return;
+// 初始化
+base.output("Initializing...");
+initialization(window => {
+    // DevTools切换
+    window.webContents.on("before-input-event", (event, input) => {
+        if (input.key == "F12" && input.type == "keyUp") {
+            window.webContents.toggleDevTools();
         }
-    }
-}, 100);
+    });
+    // 注入插件
+    window.once("ready-to-show", () => {
+        const url = window.webContents.getURL();
+        if (url.includes("app://./renderer/index.html")) {
+            // 初始化，加载BetterQQNT
+            const loader = new BetterQQNTLoader(window.webContents);
+            base.output("BetterQQNTLoader Created.");
+            loader.loadPlugins();
+            base.output("loadPlugins Loaded.");
+        }
+    });
+});
 
 
 // 继续执行QQNT启动
