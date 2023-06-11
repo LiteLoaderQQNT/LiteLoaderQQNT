b3b08d99648a1e6a0a4c5877e20724177f66fd40
沫烬染 <mojinran@outlook.com>
2023-06-11 17:06:28 +0800

    修改渲染进程插件加载器路径

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ observeNewBrowserWindow(window => {
 
     // 渲染进程PluginLoader
     window.webContents.on("dom-ready", () => {
-        const file_path = path.join(__dirname, "./renderer/loader.js");
+        const file_path = path.join(__dirname, "./renderer/index.js");
         fs.readFile(file_path, "utf-8", (err, data) => {
             if (err) throw err;
             window.webContents.executeJavaScript(data, true);
