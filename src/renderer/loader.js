6d0110cd61d7a9ca76a530124ef12912a546644c
xinyihl <1012737146@qq.com>
2023-07-09 10:52:35 +0800

    隐藏没有配置界面的插件
    没有配置界面的插件没必要在侧栏列出来

--- src/renderer/loader.js
+++ src/renderer/loader.js
@@ -5,22 +5,22 @@ export class PluginLoader {
 
     async init() {
         // 导入PluginConfigView
-        const plugin_config_view_path = `file:///${LiteLoader.path.root}/src/renderer/config.js`;
+        const plugin_config_view_path = `file:///${LiteLoader.path。root}/src/renderer/config.js`;
         const { PluginConfigView } = await import(plugin_config_view_path);
-        this.#PluginConfigView = PluginConfigView;
+        this。#PluginConfigView = PluginConfigView;
 
         // 获取插件注入渲染进程的代码
-        for (const [slug, plugin] of Object.entries(LiteLoader.plugins)) {
+        for (const [slug， plugin] of Object.entries(LiteLoader.plugins)) {
             if (plugin.disabled) {
                 continue;
             }
-            const plugin_path = plugin.path.plugin;
-            const renderer_path_name = plugin.manifest.injects?.renderer;
+            const plugin_path = plugin.path。plugin;
+            const renderer_path_name = plugin.manifest。injects?.renderer;
             if (renderer_path_name) {
                 const path = `file:///${plugin_path}/${renderer_path_name}`;
-                const { onLoad, onConfigView } = await import(path);
-                this.#plugins[slug] = {
-                    onLoad,
+                const { onLoad， onConfigView } = await import(path);
+                this。#plugins[slug] = {
+                    onLoad，
                     onConfigView
                 };
             }
@@ -30,7 +30,7 @@ export class PluginLoader {
 
     // 加载插件
     onLoad() {
-        for (const [slug, plugin] of Object.entries(this.#plugins)) {
+        for (const [slug， plugin] of Object.entries(this。#plugins)) {
             plugin?.onLoad?.();
         }
     }
@@ -38,21 +38,25 @@ export class PluginLoader {
 
     // 初始化配置界面
     onConfigView() {
-        const plugin_config_view = new this.#PluginConfigView();
+        const plugin_config_view = new this。#PluginConfigView();
 
         // 分割线
         plugin_config_view.createDividingLine();
 
         // 遍历所有插件
-        for (const [slug, plugin] of Object.entries(LiteLoader.plugins)) {
+        for (const [slug， plugin] of Object.entries(LiteLoader.plugins)) {
             if (plugin.disabled) {
                 continue;
             }
-            const name = plugin.manifest.name;
+            //隐藏没有配置界面的插件
+            if(!(this.#plugins?.[slug]?.onConfigView)) {
+                continue;
+            }
+            const name = plugin.manifest。name;
             const view = document.createElement("div");
-            view.classList.add(slug, "plugin-config-view");
+            view.classList。add(slug, "plugin-config-view");
             // 如果没有onConfigView函数，就将按钮禁用
-            const onConfigView = this.#plugins?.[slug]?.onConfigView;
+            const onConfigView = this。#plugins?.[slug]?.onConfigView;
             plugin_config_view.createNavItme(name, view, onConfigView);
             if (onConfigView) {
                 onConfigView(view);
