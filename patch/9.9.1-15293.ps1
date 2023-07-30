66792319421e95925f91227a531427848bfb6936
沫烬染 <mojinran@outlook.com>
2023-07-30 13:46:35 +0800

    修复通过脚本patch qq后主界面空白

--- patch/9.9.1-15293.ps1
+++ patch/9.9.1-15293.ps1
@@ -11,10 +11,8 @@ if (!(Test-Path $sourceFilePath)) {
 }
 
 # 要查找和替换的字节序列
-$oldBytes1 = [byte[]] @(0xE8, 0x91, 0xC2, 0xFF, 0xFF)
-$oldBytes2 = [byte[]] @(0x0F, 0x84, 0x9F, 0x04, 0x00, 0x00)
-$newBytes1 = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90)
-$newBytes2 = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
+$oldBytes = [byte[]] @(0xE8, 0x91, 0xC2, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x9F, 0x04, 0x00, 0x00)
+$newBytes = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
 
 # 使用二进制文件流打开原始文件和目标文件
 $sourceStream = [System.IO.File]::Open($sourceFilePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read)
@@ -40,20 +38,34 @@ while ($true) {
 
     # 查找并替换字节序列（写成这样子是为了提高性能）
     for ($i = 0; $i -lt $readBytes; $i++) {
-        if ($buffer[$i] -eq $oldBytes1[0] -and $buffer[$i+1] -eq $oldBytes1[1] -and $buffer[$i+2] -eq $oldBytes1[2] -and $buffer[$i+3] -eq $oldBytes1[3] -and $buffer[$i+4] -eq $oldBytes1[4]) {
-            $buffer[$i] = $newBytes1[0]
-            $buffer[$i+1] = $newBytes1[1]
-            $buffer[$i+2] = $newBytes1[2]
-            $buffer[$i+3] = $newBytes1[3]
-            $buffer[$i+4] = $newBytes1[4]
-        }
-        if ($buffer[$i] -eq $oldBytes2[0] -and $buffer[$i+1] -eq $oldBytes2[1] -and $buffer[$i+2] -eq $oldBytes2[2] -and $buffer[$i+3] -eq $oldBytes2[3] -and $buffer[$i+4] -eq $oldBytes2[4] -and $buffer[$i+5] -eq $oldBytes2[5]) {
-            $buffer[$i] = $newBytes2[0]
-            $buffer[$i+1] = $newBytes2[1]
-            $buffer[$i+2] = $newBytes2[2]
-            $buffer[$i+3] = $newBytes2[3]
-            $buffer[$i+4] = $newBytes2[4]
-            $buffer[$i+5] = $newBytes2[5]
+        if (
+            $buffer[$i + 0] -eq $oldBytes[0] -and
+            $buffer[$i + 1] -eq $oldBytes[1] -and
+            $buffer[$i + 2] -eq $oldBytes[2] -and
+            $buffer[$i + 3] -eq $oldBytes[3] -and
+            $buffer[$i + 4] -eq $oldBytes[4] -and
+            $buffer[$i + 5] -eq $oldBytes[5] -and
+            $buffer[$i + 6] -eq $oldBytes[6] -and
+            $buffer[$i + 7] -eq $oldBytes[7] -and
+            $buffer[$i + 8] -eq $oldBytes[8] -and
+            $buffer[$i + 9] -eq $oldBytes[9] -and
+            $buffer[$i + 10] -eq $oldBytes[10] -and
+            $buffer[$i + 11] -eq $oldBytes[11] -and
+            $buffer[$i + 12] -eq $oldBytes[12]
+        ) {
+            $buffer[$i + 0] = $newBytes[0]
+            $buffer[$i + 1] = $newBytes[1]
+            $buffer[$i + 2] = $newBytes[2]
+            $buffer[$i + 3] = $newBytes[3]
+            $buffer[$i + 4] = $newBytes[4]
+            $buffer[$i + 5] = $newBytes[5]
+            $buffer[$i + 6] = $newBytes[6]
+            $buffer[$i + 7] = $newBytes[7]
+            $buffer[$i + 8] = $newBytes[8]
+            $buffer[$i + 9] = $newBytes[9]
+            $buffer[$i + 10] = $newBytes[10]
+            $buffer[$i + 11] = $newBytes[11]
+            $buffer[$i + 12] = $newBytes[12]
         }
     }
 
