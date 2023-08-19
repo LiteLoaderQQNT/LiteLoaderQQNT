c68b5d6b593b2ef59a8236787224a2233e2251d0
沫烬染 <mojinran@outlook.com>
2023-08-19 16:07:27 +0800

    将安装路径获取方式改为process.execPath

--- src/main/base.js
+++ src/main/base.js
@@ -1,50 +1,10 @@
 const { app, ipcMain } = require("electron");
-const { execSync } = require("child_process")
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
 
 
-// 读取注册表
-function readRegistryValue(keyPath, valueName) {
-    try {
-        const command = `reg query "${keyPath}" /v "${valueName}"`;
-        const stdout = execSync(command, { stdio: 'pipe' }).toString();
-        const lines = stdout.split('\r\n');
-        const valueLine = lines.find(line => line.includes(valueName));
-        if (valueLine) {
-            const parts = valueLine.split(/\s+/);
-            return path.dirname(parts.slice(3).join(' '));
-        }
-    }
-    catch (error) {
-        //console.error(`Error reading registry: ${error}`);
-        //此处抛异常是正常的，因为有两种路径，总有一个是读取不到的。
-    }
-    return null;
-}
-
-
-function getQQInstallDir() {
-    //Windows 则暴力拉取注册表
-    if (os.platform() === "win32") {
-        const reg = "HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\QQ";
-        const reg2 = "HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\QQ";
-        return readRegistryValue(reg, "UninstallString") || readRegistryValue(reg2, "UninstallString");
-    }
-    else {
-        /*
-        ? OS X 和 Linux 不同发行版获取QQ安装路径的方法各不相同，
-        ? 故 Linux 和 OS X 都先写死为相对路径，待日后解决该问题，这并不是一个非常急迫的需求。
-        const stdout = execSync(`which qq`, { stdio: 'pipe' }).toString();
-        return path.dirname(stdout);
-        */
-        return path.join(__dirname, "../../../../../");
-    }
-}
-
-
-const qq_install_dir = getQQInstallDir();
+const qq_install_dir = path.join(process.execPath, "../");
 const liteloader_package = require("../../package.json");
 const qqnt_package = require(`${qq_install_dir}/resources/app/package.json`);
 
