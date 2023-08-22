46318d01585cd37b14af11cb99f6a1a1571d2c36
mo-jinran <mojinran@outlook.com>
2023-08-22 13:11:41 +0800

    使用 _linkedBinding 获取 major.node 引入

--- src/preload/hook.js
+++ src/preload/hook.js
@@ -1,20 +1,31 @@
-const { contextBridge } = require("electron");
-
-
-const majorPath = (() => {
-    if (process.platform == "win32") {
-        return "./major.node";
+// 兼容新旧版本与多平台，获取 major.node 引入
+const major = (() => {
+    // 新版本需要使用 反射 获取 _linkedBinding 从而引入
+    try {
+        const { _linkedBinding } = Reflect.get(global, "process");
+        return _linkedBinding("major")
     }
-    if (process.platform == "linux") {
-        return `${process.execPath}/../resources/app/major.node`;
-    }
-    if (process.platform == "darwin") {
-        return `${process.execPath}/../../../../../Resources/app/major.node`;
+    // 老版本没有限制所以直接引入
+    catch {
+        if (process.platform == "win32") {
+            const major_path = `${process.execPath}/../resources/app/major.node`;
+            return require(major_path);
+        }
+        if (process.platform == "linux") {
+            const major_path = `${process.execPath}/../resources/app/major.node`;
+            return require(major_path);
+        }
+        if (process.platform == "darwin") {
+            const major_path = `${process.execPath}/../../../../../Resources/app/major.node`;
+            return require(major_path);
+        }
     }
 })();
 
+const { contextBridge } = require("electron");
+
 contextBridge.exposeInMainWorld("electron", {
-    load: (file) => require(majorPath).load(file, module)
+    load: (file) => major.load(file, module)
 });
 
-require(majorPath).load("p_preload", module);
+major.load("p_preload", module);
