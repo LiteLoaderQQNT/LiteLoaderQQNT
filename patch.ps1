c0ead6d4d24ca6103d6ef82abe241a347aac044c
xh321 <xiaohe321@outlook.com>
2023-08-25 02:31:44 +0800

    更新Patch脚本，现在变得通用化，无需再选择QQ版本，无需手动修改package.json #129

--- patch.ps1
+++ patch.ps1
@@ -2,12 +2,6 @@
 Add-Type -AssemblyName PresentationCore
 Add-Type -AssemblyName PresentationFramework
 
-
-#顶部区域 - 列表
-$listbox = New-Object System.Windows.Controls.ListBox
-$listbox.Margin = New-Object System.Windows.Thickness(10, 10, 10, 10)
-
-
 #底部区域 - 进度条
 $progressbar = New-Object System.Windows.Controls.ProgressBar
 $progressbar.Margin = New-Object System.Windows.Thickness(10, 0, 10, 10)
@@ -19,8 +13,8 @@ $progressbar.Value = 0
 #底部区域 - 按钮
 $button = New-Object System.Windows.Controls.Button
 $button.Margin = New-Object System.Windows.Thickness(10, 0, 10, 10)
-$button.Content = "请在上方选择对应版本的 QQ！"
-$button.IsEnabled = $false
+$button.Content = "开始执行补丁"
+$button.IsEnabled = $true
 
 
 #底部区域 - 标签
@@ -42,12 +36,10 @@ $row_bottom.Height = New-Object System.Windows.GridLength(40)
 $grid.RowDefinitions.Add($row_top)
 $grid.RowDefinitions.Add($row_bottom)
 
-$grid.Children.Add($listbox)
 $grid.Children.Add($progressbar)
 $grid.Children.Add($textblock)
 $grid.Children.Add($button)
 
-[System.Windows.Controls.Grid]::SetRow($listbox, 0)
 [System.Windows.Controls.Grid]::SetRow($progressbar, 1)
 [System.Windows.Controls.Grid]::SetRow($textblock, 1)
 [System.Windows.Controls.Grid]::SetRow($button, 1)
@@ -66,57 +58,14 @@ $window.UseLayoutRounding = $true
 [System.Windows.Media.TextOptions]::SetTextRenderingMode($window, [System.Windows.Media.TextRenderingMode]::ClearType)
 
 
-# patch 对照表
-$patch_hashtable = [ordered] @{
-    "9.9.1-15293_x64" = @{
-        "original" = [byte[]] @(0xE8, 0x91, 0xC2, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x9F, 0x04, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.1-15293_x86" = @{
-        "original" = [byte[]] @(0xE8, 0x75, 0xCC, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0xC6, 0x02, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.1-15489_x64" = @{
-        "original" = [byte[]] @(0xE8, 0x91, 0xC2, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x9F, 0x04, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.1-15489_x86" = @{
-        "original" = [byte[]] @(0xE8, 0x75, 0xCC, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0xC6, 0x02, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.1-15820_x64" = @{
-        "original" = [byte[]] @(0xE8, 0x96, 0xC2, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x04, 0x04, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.1-15820_x86" = @{
-        "original" = [byte[]] @(0xE8, 0x79, 0xCC, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x1E, 0x02, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.2-15962_x64" = @{
-        "original" = [byte[]] @(0xE8, 0x96, 0xC2, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x04, 0x04, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.2-15962_x86" = @{
-        "original" = [byte[]] @(0xE8, 0x79, 0xCC, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x1E, 0x02, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.2-16183_x64" = @{
-        "original" = [byte[]] @(0xE8, 0xC6, 0xC9, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x04, 0x04, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-    "9.9.2-16183_x86" = @{
-        "original" = [byte[]] @(0xE8, 0xA9, 0xD2, 0xFF, 0xFF, 0x84, 0xC0, 0x0F, 0x84, 0x1E, 0x02, 0x00, 0x00)
-        "replace"  = [byte[]] @(0x90, 0x90, 0x90, 0x90, 0x90, 0x84, 0xC0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90)
-    }
-}
-
-
 # Patch QQ
 $qq_pather = {
     param(
         $path,
-        $original_bytes,
-        $replace_bytes,
+        $original_bytes1,
+        $replace_bytes1,
+        $original_bytes2,
+        $replace_bytes2,
         $updateProgress,
         $finish
     )
@@ -140,7 +89,7 @@ $qq_pather = {
 
     $fileSize = $sourceStream.Length
     $bytesProcessed = 0
-    $matched = $false
+    $matched = 0
 
     Invoke-Command -ScriptBlock $updateProgress -ArgumentList 0
 
@@ -154,27 +103,46 @@ $qq_pather = {
             break
         }
 
-        if (-not $matched) {
+        if ($matched -ne 2) {
             # 查找并替换字节序列（写成这样子是为了提高性能）
             for ($i = 0; $i -lt $readBytes; $i++) {
                 if (
-                    $buffer[$i + 0] -eq $original_bytes[0] -and
-                    $buffer[$i + 1] -eq $original_bytes[1] -and
-                    $buffer[$i + 2] -eq $original_bytes[2] -and
-                    $buffer[$i + 3] -eq $original_bytes[3] -and
-                    $buffer[$i + 4] -eq $original_bytes[4] -and
-                    $buffer[$i + 5] -eq $original_bytes[5] -and
-                    $buffer[$i + 6] -eq $original_bytes[6] -and
-                    $buffer[$i + 7] -eq $original_bytes[7] -and
-                    $buffer[$i + 8] -eq $original_bytes[8] -and
-                    $buffer[$i + 9] -eq $original_bytes[9] -and
-                    $buffer[$i + 10] -eq $original_bytes[10] -and
-                    $buffer[$i + 11] -eq $original_bytes[11] -and
-                    $buffer[$i + 12] -eq $original_bytes[12]
+                    $buffer[$i + 0] -eq $original_bytes1[0] -and
+                    $buffer[$i + 1] -eq $original_bytes1[1] -and
+                    $buffer[$i + 2] -eq $original_bytes1[2] -and
+                    $buffer[$i + 3] -eq $original_bytes1[3] -and
+                    $buffer[$i + 4] -eq $original_bytes1[4] -and
+                    $buffer[$i + 5] -eq $original_bytes1[5] -and
+                    $buffer[$i + 6] -eq $original_bytes1[6] -and
+                    $buffer[$i + 7] -eq $original_bytes1[7] -and
+                    $buffer[$i + 8] -eq $original_bytes1[8] -and
+                    $buffer[$i + 9] -eq $original_bytes1[9] -and
+                    $buffer[$i + 10] -eq $original_bytes1[10] -and
+                    $buffer[$i + 11] -eq $original_bytes1[11] -and
+                    $buffer[$i + 12] -eq $original_bytes1[12]
                 ) {
-                    $matched = $true
+                    $matched = $matched + 1
                     for ($j = 0; $j -lt 13; $j++) {
-                        $buffer[$i + $j] = $replace_bytes[$j]
+                        $buffer[$i + $j] = $replace_bytes1[$j]
+                    }
+                } elseif  (
+                    $buffer[$i + 0] -eq $original_bytes2[0] -and
+                    $buffer[$i + 1] -eq $original_bytes2[1] -and
+                    $buffer[$i + 2] -eq $original_bytes2[2] -and
+                    $buffer[$i + 3] -eq $original_bytes2[3] -and
+                    $buffer[$i + 4] -eq $original_bytes2[4] -and
+                    $buffer[$i + 5] -eq $original_bytes2[5] -and
+                    $buffer[$i + 6] -eq $original_bytes2[6] -and
+                    $buffer[$i + 7] -eq $original_bytes2[7] -and
+                    $buffer[$i + 8] -eq $original_bytes2[8] -and
+                    $buffer[$i + 9] -eq $original_bytes2[9] -and
+                    $buffer[$i + 10] -eq $original_bytes2[10] -and
+                    $buffer[$i + 11] -eq $original_bytes2[11] -and
+                    $buffer[$i + 12] -eq $original_bytes2[12]
+                ) {
+                    $matched = $matched + 1
+                    for ($j = 0; $j -lt 13; $j++) {
+                        $buffer[$i + $j] = $replace_bytes2[$j]
                     }
                 }
             }
@@ -196,6 +164,14 @@ $qq_pather = {
     Invoke-Command -ScriptBlock $finish
 }
 
+Function replace_in_file($Filename, $Oldvalue, $Newvalue)
+{
+    if (Test-Path $Filename) {
+        $text = [IO.File]::ReadAllText($Filename) -replace $Oldvalue, $Newvalue
+        [IO.File]::WriteAllText($Filename, $text)
+    }
+}
+
 
 # 获取QQ安装位置
 $qqDirPath = {
@@ -211,48 +187,29 @@ $qqDirPath = {
     return $filePath.Substring(0, $filePath.LastIndexOf("\"))
 }
 
-
-#顶部区域 - 列表
-foreach ($name in $patch_hashtable.Keys) {
-    $listbox.Items.Add($name)
-}
-
-# 列表条目选择事件
-$listbox.Add_SelectionChanged(
-    {
-        $selectedItem = $listbox.SelectedItem
-        $button.IsEnabled = $selectedItem
-        if ($selectedItem -ne $null) {
-            $button.Content = "已选择 $selectedItem 版本，点击此按钮开始 Patch".Replace("_", "__")
-        }
-        else {
-            $button.Content = "请在上方选择对应版本的 QQ！"
-        }
-    }
-)
-
 # 判断列表中是否存在匹配项
 $path = & $qqDirPath
-$eleArch = (Get-Content -Path ($path + "\resources\app\package.json") -Raw | ConvertFrom-Json).eleArch
-$curVersion = (Get-Content -Path ($path + "\resources\app\versions\config.json") -Raw | ConvertFrom-Json).curVersion
-$matchingString = "${curVersion}_${eleArch}"
-
-# 根据文件信息和位数构建要匹配的字符串
-if ($matchingString -in $patch_hashtable.Keys) {
-    $matchingItem = $listbox.Items | Where-Object { $_ -eq $matchingString }
-    $listbox.SelectedItem = $matchingItem
-}
-
 
 #底部区域 - 按钮
 $button.Add_Click(
     {
         $button.Visibility = [Windows.Visibility]::Hidden
-        $listbox.IsEnabled = $false
         $textblock.Text = "正在初始化..."
 
-        $original_bytes = $patch_hashtable[$listbox.SelectedItem].original
-        $replace_bytes = $patch_hashtable[$listbox.SelectedItem].replace
+        $original_bytes1 =  [byte[]] @(0xAD, 0xA0, 0xB4, 0xAF, 0xA2, 0xA9, 0xA4, 0xB3, 0xEF, 0xAB, 0xB2, 0xAE, 0xAF, 0x00)
+        $replace_bytes1 =  [byte[]] @(0xA3, 0xA0, 0xB4, 0xAF, 0xA2, 0xA9, 0xA4, 0xB3, 0xEF, 0xAB, 0xB2, 0xAE, 0xAF, 0x00)
+
+        $original_bytes2 =  [byte[]] @(0xB1, 0xA0, 0xA2, 0xAA, 0xA0, 0xA6, 0xA4, 0xEF, 0xAB, 0xB2, 0xAE, 0xAF, 0x00)
+        $replace_bytes2 =   [byte[]] @(0xA3, 0xA0, 0xA2, 0xAA, 0xA0, 0xA6, 0xA4, 0xEF, 0xAB, 0xB2, 0xAE, 0xAF, 0x00)
+
+        if(-not (Test-Path ($path + "\resources\app\backage.json"))){
+            Copy-Item -Path ($path + "\resources\app\package.json") -Destination ($path + "\resources\app\backage.json") -Force
+        }
+        Copy-Item -Path ($path + "\resources\app\launcher.json") -Destination ($path + "\resources\app\bauncher.json") -Force
+
+        replace_in_file ($path + "\resources\app\bauncher.json") "package.json" "backage.json"
+        replace_in_file ($path + "\resources\app\package.json") "./app_launcher/index.js" "LiteLoader"
+
         $updateProgress = {
             param ($progress)
             $window.Dispatcher.Invoke(
@@ -268,7 +225,6 @@ $button.Add_Click(
                     $textblock.Text = "Patch 完成！"
                     $popup = new-object -comobject wscript.shell
                     $popup.popup("Patch 完成！")
-                    $listbox.IsEnabled = $true
                     $button.Visibility = [Windows.Visibility]::Visible
                     $textblock.Text = ""
                     $progressbar.Value = 0
@@ -282,8 +238,10 @@ $button.Add_Click(
         $powershell.Runspace = $runspace
         $powershell.AddScript($qq_pather)
         $powershell.AddArgument($path)
-        $powershell.AddArgument($original_bytes)
-        $powershell.AddArgument($replace_bytes)
+        $powershell.AddArgument($original_bytes1)
+        $powershell.AddArgument($replace_bytes1)
+        $powershell.AddArgument($original_bytes2)
+        $powershell.AddArgument($replace_bytes2)
         $powershell.AddArgument($updateProgress)
         $powershell.AddArgument($finish)
         $powershell.BeginInvoke()
