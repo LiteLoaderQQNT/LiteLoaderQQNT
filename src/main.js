946abb0a1d9d2f53698b964cdc1545d18774b2ee
沫烬染 <mojinran@outlook.com>
2024-04-02 00:57:05 +0800

    修复小程序无法使用

--- src/main.js
+++ src/main.js
@@ -32,6 +32,40 @@ const loader = (new class {
 }).init();
 
 
+ipcMain.handle("LiteLoader.LiteLoader.preload", (event) => {
+    const qqnt_preload_path = event.sender.preload;
+    return fs.readFileSync(qqnt_preload_path, "utf-8");
+});
+
+
+function processPreloadPath(qqnt_preload_path) {
+    const liteloader_preload_path = path.join(LiteLoader.path.root, "src/preload.js");
+    const asar_path = qqnt_preload_path.split(".asar")[0];
+
+    let preload_path = "";
+
+    if (asar_path == qqnt_preload_path) {
+        const preload_path_dirname = path.dirname(qqnt_preload_path).replaceAll("\\", "/");
+        preload_path = `${preload_path_dirname}/../application/preload.js`;
+    }
+    else {
+        const asar_path_dirname = path.dirname(qqnt_preload_path.split(".asar")[1]);
+        preload_path = `${asar_path}/../${path.basename(asar_path)}${asar_path_dirname}/preload.js`;
+    }
+
+    if (!fs.existsSync(preload_path)) {
+        fs.mkdirSync(path.dirname(preload_path), { recursive: true });
+        fs.copyFileSync(liteloader_preload_path, preload_path);
+    }
+
+    if (fs.readFileSync(preload_path, "utf-8") != fs.readFileSync(liteloader_preload_path, "utf-8")) {
+        fs.copyFileSync(liteloader_preload_path, preload_path);
+    }
+
+    return preload_path;
+}
+
+
 // 注册协议
 function protocolRegister(protocol) {
     if (!protocol.isProtocolRegistered("local")) {
@@ -47,41 +81,29 @@ function protocolRegister(protocol) {
     }
 }
 
-const liteloader_preload_path = path.join(LiteLoader.path.root, "src/preload.js");
-ipcMain.handle("LiteLoader.LiteLoader.preload", (event) => {
-    const qqnt_preload_path = event.sender.preload;
-    return fs.readFileSync(qqnt_preload_path, "utf-8");
-})
 
 function proxyBrowserWindowConstruct(target, [config], newTarget) {
     const qqnt_preload_path = config.webPreferences.preload;
-    const preload_path = `${path.dirname(qqnt_preload_path).replaceAll("\\", "/")}/../application/preload.js`;
-
-    if (!fs.existsSync(preload_path)) {
-        fs.mkdirSync(path.dirname(preload_path), { recursive: true });
-        fs.copyFileSync(liteloader_preload_path, preload_path);
-    }
-
-    if (fs.readFileSync(preload_path, "utf-8") != fs.readFileSync(liteloader_preload_path, "utf-8")) {
-        fs.copyFileSync(liteloader_preload_path, preload_path);
-    }
-
-    const new_config = {
-        ...config,
-        webPreferences: {
-            ...config.webPreferences,
-            webSecurity: false,
-            devTools: true,
-            preload: preload_path,
-            additionalArguments: ["--fetch-schemes=local"]
+    const window = Reflect.construct(target, [
+        {
+            ...config,
+            webPreferences: {
+                ...config.webPreferences,
+                webSecurity: false,
+                devTools: true,
+                preload: processPreloadPath(qqnt_preload_path),
+                additionalArguments: ["--fetch-schemes=local"]
+            }
         }
-    }
+    ], newTarget);
 
-    const window = Reflect.construct(target, [new_config], newTarget);
+    // 挂载窗口原preload
     window.webContents.preload = qqnt_preload_path;
 
     //加载自定义协议
     protocolRegister(window.webContents.session.protocol);
+
+    // 加载插件
     loader.onBrowserWindowCreated(window);
 
     return window;
