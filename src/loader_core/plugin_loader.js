61b9e82e04bd729a8d93c446e523a5ab894e2b7e
沫烬染 <mo_jinran@outlook.com>
2024-08-12 18:10:35 +0800

    重构插件加载代码

--- src/loader_core/plugin_loader.js
+++ src/loader_core/plugin_loader.js
@@ -1,39 +1,40 @@
-const default_config = require("../settings/static/config.json");
+const { app, dialog } = require("electron");
 const path = require("node:path");
 const fs = require("node:fs");
 
+const default_config = require("../settings/static/config.json");
 const launcher_node = path.join(process.resourcesPath, "app/app_launcher/launcher.node");
 require(launcher_node).load("external_admzip", module);
+
 
 const output = (...args) => console.log("\x1b[32m%s\x1b[0m", "[LiteLoader]", ...args);
 const config = LiteLoader.api.config.get("LiteLoader", default_config);
 
-// 如果设定环境变量
-if (process.env?.LITELOADERQQNT_PROFILE) {
-    output("Use LITELOADERQQNT_PROFILE environment variables:", LiteLoader.path.profile);
-}
 
-// 如果插件全局开关为关闭状态
-if (!config.enable_plugins) {
-    output("Plugin loader is disabled, no plugins will be loaded.");
-    return;
-}
-
-output("Start finding all plugins.");
-
-for (const slug in config.deleting_plugins) {
+function deletePlugin(slug) {
     try {
         const { plugin_path, data_path } = config.deleting_plugins[slug];
         if (data_path) fs.rmSync(data_path, { recursive: true });
         fs.rmSync(plugin_path, { recursive: true });
+    }
+    catch (error) {
+        output("Deleting Plugin Error", error);
+        app.on("ready", () => {
+            dialog.showMessageBox(null, {
+                type: "error",
+                title: "LiteLoaderQQNT",
+                message: `删除插件时报错，请检查并手动删除\n${error}`
+            });
+        });
+    }
+    finally {
         delete config.deleting_plugins[slug];
         LiteLoader.api.config.set("LiteLoader", config);
-    } catch (error) {
-        console.log(error);
     }
 }
 
-for (const slug in config.installing_plugins) {
+
+function InstallPlugin(slug) {
     try {
         const { plugin_path, plugin_type } = config.installing_plugins[slug];
         const dest_path = path.join(LiteLoader.path.plugins, slug);
@@ -46,77 +47,109 @@ for (const slug in config.installing_plugins) {
         if (plugin_type == "json") {
             fs.cpSync(path.dirname(plugin_path), dest_path, { recursive: true });
         }
+    }
+    catch (error) {
+        output("Installing Plugin Error", error);
+        app.on("ready", () => {
+            dialog.showMessageBox(null, {
+                type: "error",
+                title: "LiteLoaderQQNT",
+                message: `安装插件时报错，请检查并手动安装\n${error}`
+            });
+        });
+    }
+    finally {
         delete config.installing_plugins[slug];
         LiteLoader.api.config.set("LiteLoader", config);
-    } catch (error) {
-        console.log(error);
     }
 }
 
-// 读取插件目录
-try {
-    for (const plugin_pathname of fs.readdirSync(LiteLoader.path.plugins, "utf-8")) {
-        try {
-            const manifest_file = path.join(LiteLoader.path.plugins, plugin_pathname, "manifest.json");
-            const manifest = JSON.parse(fs.readFileSync(manifest_file, "utf-8"));
 
-            const incompatible_version = Number(manifest.manifest_version) != 4;
-            const incompatible_platform = !manifest.platform.includes(LiteLoader.os.platform);
-            const disabled_plugin = config.disabled_plugins.includes(manifest.slug);
+function findAllPlugin() {
+    const plugins = [];
+    try {
+        for (const pathname of fs.readdirSync(LiteLoader.path.plugins, "utf-8")) {
+            try {
+                const filepath = path.join(LiteLoader.path.plugins, pathname, "manifest.json");
+                const manifest = JSON.parse(fs.readFileSync(filepath, "utf-8"));
+                if (manifest.manifest_version == 4) plugins.push({ pathname, manifest });
+            }
+            catch { continue }
+        }
+    }
+    catch (error) {
+        output("Find Plugin Error", error);
+        app.on("ready", () => {
+            dialog.showMessageBox(null, {
+                type: "warning",
+                title: "LiteLoaderQQNT",
+                message: `在读取数据目录时报错了！请检查插件目录或忽略继续启动\n${error}`
+            });
+        });
+    }
+    return plugins;
+}
 
-            const plugin_path = path.join(LiteLoader.path.plugins, plugin_pathname);
-            const data_path = path.join(LiteLoader.path.data, manifest.slug);
 
-            const main_file = path.join(plugin_path, manifest?.injects?.main ?? "");
-            const preload_file = path.join(plugin_path, manifest?.injects?.preload ?? "");
-            const renderer_file = path.join(plugin_path, manifest?.injects?.renderer ?? "");
-
-            LiteLoader.plugins[manifest.slug] = {
-                manifest: manifest,
-                incompatible: incompatible_version || incompatible_platform,
-                disabled: disabled_plugin,
-                path: {
-                    plugin: plugin_path,
-                    data: data_path,
-                    injects: {
-                        main: fs.statSync(main_file).isFile() ? main_file : null,
-                        preload: fs.statSync(preload_file).isFile() ? preload_file : null,
-                        renderer: fs.statSync(renderer_file).isFile() ? renderer_file : null
-                    }
-                }
+function getPluginInfo(pathname, manifest) {
+    const incompatible_platform = !manifest.platform.includes(LiteLoader.os.platform);
+    const disabled_plugin = config.disabled_plugins.includes(manifest.slug);
+    const plugin_path = path.join(LiteLoader.path.plugins, pathname);
+    const data_path = path.join(LiteLoader.path.data, manifest.slug);
+    const main_file = path.join(plugin_path, manifest?.injects?.main ?? "");
+    const preload_file = path.join(plugin_path, manifest?.injects?.preload ?? "");
+    const renderer_file = path.join(plugin_path, manifest?.injects?.renderer ?? "");
+    return {
+        manifest: manifest,
+        incompatible: incompatible_platform,
+        disabled: disabled_plugin,
+        path: {
+            plugin: plugin_path,
+            data: data_path,
+            injects: {
+                main: fs.statSync(main_file).isFile() ? main_file : null,
+                preload: fs.statSync(preload_file).isFile() ? preload_file : null,
+                renderer: fs.statSync(renderer_file).isFile() ? renderer_file : null
             }
         }
-        catch {
-            continue;
-        }
     }
-}
-catch {
-    output("Failed to read plugins directory!");
-    output("Please check the plugins directory.");
 }
 
-// 输出插件状态
-for (const plugin of Object.values(LiteLoader.plugins)) {
-    const dependencies = plugin.manifest?.dependencies?.filter(slug => !LiteLoader.plugins[slug]);
-    // 输出不兼容
-    if (plugin.incompatible) {
-        output("Found plugin:", plugin.manifest.name, "(Incompatible)");
-        continue;
+
+function loadAllPlugin() {
+    const plugins = findAllPlugin();
+    const dependencies = new Set();
+    for (const { pathname, manifest } of plugins) {
+        output("Found Plugin:", manifest.name);
+        LiteLoader.plugins[manifest.slug] = getPluginInfo(pathname, manifest);
+        manifest.dependencies?.forEach?.(slug => dependencies.add(slug));
     }
-    // 输出已禁用
-    if (plugin.disabled) {
-        output("Found plugin:", plugin.manifest.name, "(Disabled)");
-        continue;
+    const slugs = plugins.map(plugin => plugin.manifest.slug);
+    const missing = [...dependencies].filter(slug => !slugs.includes(slug));
+    for (const slug of missing) {
+        output("Missing Plugin:", slug);
+        app.on("ready", () => {
+            dialog.showMessageBox(null, {
+                type: "warning",
+                title: "LiteLoaderQQNT",
+                message: `插件缺少依赖：${slug}`
+            });
+        });
     }
-    // 缺少依赖
-    if (dependencies?.length) {
-        output("Found plugin:", plugin.manifest.name, "Missing dependencies:", dependencies.toString());
-        continue;
-    }
-    output("Found plugin:", plugin.manifest.name);
 }
 
-// 输出找到了多少插件
-const plugins_length = Object.keys(LiteLoader.plugins).length;
-output(`Found ${plugins_length} plugins,`, plugins_length ? "start loading plugins." : "no plugin to be loaded.");
+
+// 删除插件
+for (const slug in config.deleting_plugins) {
+    deletePlugin(slug);
+}
+
+// 安装插件
+for (const slug in config.installing_plugins) {
+    InstallPlugin(slug);
+}
+
+// 加载插件
+if (config.enable_plugins) {
+    loadAllPlugin();
+}
