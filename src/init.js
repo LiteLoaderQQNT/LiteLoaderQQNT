89277965dc46a370842aac019a4898560aac028f
沫烬染 <mo_jinran@outlook.com>
2024-11-05 12:06:39 +0800

    setTimeout 换成 setImmediate

--- src/init.js
+++ src/init.js
@@ -3,11 +3,11 @@ require("./loader_core/plugin_loader.js");
 require("./main.js");
 require(require("path").join(process.resourcesPath, "app/app_launcher/index.js"));
 
-setTimeout(() => {
+setImmediate(() => {
     const version = LiteLoader.package.qqnt.buildVersion;
     global.launcher.installPathPkgJson.main = (() => {
         if (version >= 29271) return "./application.asar/app_launcher/index.js";
         if (version >= 28060) return "./application/app_launcher/index.js";
         return "./app_launcher/index.js";
     })();
-}, 0);
+});
