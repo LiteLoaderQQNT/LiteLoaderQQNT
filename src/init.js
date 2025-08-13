ea6d0c7647c15bdc31f26c725c5212b5739b639a
沫烬染 <mo_jinran@outlook.com>
2025-08-13 12:47:04 +0800

    移除 34xxx 版本之前版本兼容

--- src/init.js
+++ src/init.js
@@ -3,12 +3,7 @@ require("./loader_core/plugin_loader.js");
 require("./main.js");
 
 if (!globalThis.qwqnt) {
-    const main_path = (() => {
-        const version = LiteLoader.package.qqnt.buildVersion;
-        if (version >= 29271) return "./application.asar/app_launcher/index.js";
-        if (version >= 28060) return "./application/app_launcher/index.js";
-        return "./app_launcher/index.js";
-    })();
+    const main_path = "./application.asar/app_launcher/index.js";
     require(require("path").join(process.resourcesPath, "app", main_path));
     setImmediate(() => global.launcher.installPathPkgJson.main = main_path);
 }
